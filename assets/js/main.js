fb005de926adf14d8b2ce7f1ca377749e4f3b6e1
algorweb <goroshchenko.a@gmail.com>
2018-09-18 17:12:19 +0300

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,62 @@
+var $body,
+	windowHeight,
+	windowWidth,
+	degree = 0.0174532925,
+	mediaPoint1 = 1024,
+	mediaPoint2 = 768,
+	mediaPoint3 = 480,
+	mediaPoint4 = 320;
+
+$(document).ready(function ($) {
+	$body = $('body');
+
+	//developer funcitons
+	pageWidget(['index']);
+	getAllClasses('html','.elements_list');
+});
+
+$(window).on('load', function () {
+	updateSizes();
+	loadFunc();
+});
+
+$(window).on('resize', function () {
+	resizeFunc();
+});
+
+$(window).on('scroll', function () {
+	scrollFunc();
+});
+
+function loadFunc() {
+}
+
+function resizeFunc() {
+	updateSizes();
+}
+
+function scrollFunc() {
+}
+
+function updateSizes() {
+	windowWidth = window.innerWidth;
+	windowHeight = window.innerHeight;
+}
+
+if ('objectFit' in document.documentElement.style === false) {
+	document.addEventListener('DOMContentLoaded', function () {
+		Array.prototype.forEach.call(document.querySelectorAll('img[data-object-fit]'), function (image) {
+			(image.runtimeStyle || image.style).background = 'url("' + image.src + '") no-repeat 50%/' + (image.currentStyle ? image.currentStyle['object-fit'] : image.getAttribute('data-object-fit'));
+
+			image.src = 'data:image/svg+xml,%3Csvg xmlns=\'http://www.w3.org/2000/svg\' width=\'' + image.width + '\' height=\'' + image.height + '\'%3E%3C/svg%3E';
+		});
+	});
+}
+
+function getRandomInt(min, max) {
+	return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function getRandom(min, max) {
+	return Math.random() * (max - min) + min;
+}
